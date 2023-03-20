cb73d6362c3da89aaa6fa762469452a9ebfef45a
yihnatchuk <yihnatchuk@griddynamics.com>
2023-03-20 22:48:15 +0000

    facebook ads

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import './App.css';
 function App() {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect( async() => {
-    const { default: ReactPixel } = await import('react-facebook-pixel');
-    ReactPixel.init('1273202096933750', null, {
-        autoConfig: true,
-        debug: true,
-      });
-    ReactPixel.pageView();
-    ReactPixel.track("ViewContent")
+    // const { default: ReactPixel } = await import('react-facebook-pixel');
+    // ReactPixel.init('1273202096933750', null, {
+    //     autoConfig: true,
+    //     debug: true,
+    //   });
+    // ReactPixel.pageView();
+    // ReactPixel.track("ViewContent")
   }, [])
   
   return (
