b2a55f4eeb0d4e216183e93e68ec8b0cfad31119
yihnatchuk <yihnatchuk@griddynamics.com>
2023-03-20 23:15:41 +0000

    facebook ads

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ function App() {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect( async() => {
     const { default: ReactPixel } = await import('react-facebook-pixel');
-    ReactPixel.init('577428994447337', null, {
+    ReactPixel.init('577428994447337', {
+      em: 'email@email.com',         //Values will be hashed automatically by the pixel using SHA-256
+      fn: 'first_name',    
+      ln: 'last_name',
+      country: 'country'
+    }, {
         autoConfig: true,
         debug: true,
       });
