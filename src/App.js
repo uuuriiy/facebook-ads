1bf9859f51ea2feed67d520b3ccac01a9b1b5006
yihnatchuk <yihnatchuk@griddynamics.com>
2023-03-20 12:09:56 +0000

    facebook ads

--- src/App.js
+++ src/App.js
@@ -6,15 +6,20 @@ import ReactPixel from 'react-facebook-pixel';
 // demo
 // ID
 // 527637292825855
-
+ReactPixel.init('527637292825855');
 
 function App() {
   useEffect(() => {
-    ReactPixel.init('527637292825855');
+  ReactPixel.init('527637292825855');
     ReactPixel.track('ViewContent', {
       content_name: 'Product Name',
       content_category: 'Product Category'
     });
+    ReactPixel.pageView(); // For tracking page view
+  // ReactPixel.track(event, data); // For tracking default events. More info about standard events: https://developers.facebook.com/docs/facebook-pixel/implementation/conversion-tracking#standard-events
+  // ReactPixel.trackSingle('PixelId', event, data); // For tracking default events.
+  // ReactPixel.trackCustom(event, data); // For tracking custom events. More info about custom events: https://developers.facebook.com/docs/facebook-pixel/implementation/conversion-tracking#custom-events
+  //   eactPixel.trackSingleCustom('PixelId', event, data); // For tracking 
     console.log(ReactPixel.track('ViewContent', {
       content_name: 'Product Name',
       content_category: 'Product Category'
