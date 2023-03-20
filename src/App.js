3bff5fe45e866b4cdae0049d2db320790fb24d13
yihnatchuk <yihnatchuk@griddynamics.com>
2023-03-20 12:24:43 +0000

    facebook ads

--- src/App.js
+++ src/App.js
@@ -6,16 +6,22 @@ import ReactPixel from 'react-facebook-pixel';
 // demo
 // ID
 // 527637292825855
-ReactPixel.init('527637292825855');
+
+const advancedMatching = { em: 'ihnatchukud.com' }; // optional, more info: https://developers.facebook.com/docs/facebook-pixel/advanced/advanced-matching
+const options = {
+  autoConfig: true, // set pixel's autoConfig. More info: https://developers.facebook.com/docs/facebook-pixel/advanced/
+  debug: false, // enable logs
+};
 
 function App() {
   useEffect(() => {
-  ReactPixel.init('527637292825855');
+  ReactPixel.init('527637292825855', advancedMatching, options);
     ReactPixel.track('ViewContent', {
-      content_name: 'Product Name',
-      content_category: 'Product Category'
+      content_name: 'Facebook ads',
+      content_category: 'Ads'
     });
     ReactPixel.pageView(); // For tracking page view
+    ReactPixel.revokeConsent();
   // ReactPixel.track(event, data); // For tracking default events. More info about standard events: https://developers.facebook.com/docs/facebook-pixel/implementation/conversion-tracking#standard-events
   // ReactPixel.trackSingle('PixelId', event, data); // For tracking default events.
   // ReactPixel.trackCustom(event, data); // For tracking custom events. More info about custom events: https://developers.facebook.com/docs/facebook-pixel/implementation/conversion-tracking#custom-events
@@ -29,18 +35,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <button onClick={ReactPixel.grantConsent}>Accept cookies</button>.
       </header>
     </div>
   );
